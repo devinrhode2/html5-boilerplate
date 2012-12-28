3eaa4739b2313eb20e8c9c1f3f2c4cd068fd2e7d
Devin Rhode <devinrhode2@gmail.com>
2012-12-28 04:36:09 -0600

    commented up console methods

--- js/plugins.js
+++ js/plugins.js
@@ -3,10 +3,24 @@
     var method;
     var noop = function () {};
     var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
+        'log', 'error', 'warn',
+        'dir',               // print object level view, showing prototype methods
+        'time', 'timeEnd',   // console.time('op') ... console.timeEnd('op') => 12ms
+        'trace',             // print current stack
+        'assert',            // oh plz (takes boolean)
+        'group', 'groupEnd', // place logs in a collapsable unit
+        'groupCollapsed',    // same as group, but opens as collapsed by default (logs will be hidden)
+
+        'profile', 'profileEnd', // oh sure these methods don't get a good comment
+
+        //lmgtfy.com/?q=console.count
+        'count', 'timeStamp', 'markTimeline',
+
+        // identical to console.log (in chrome)
+        'info', 'debug', 'dirxml'
+
+        // Not defined in chrome
+        'clear', 'exception', 'table'
     ];
     var length = methods.length;
     var console = (window.console = window.console || {});
